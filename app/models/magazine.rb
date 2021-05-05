6c2361562222ad4f0d16c9c3350434c988804c14
esellers25 <erin.e.sellers@gmail.com>
2021-05-05 11:27:18 -0400

    deliverables met

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,13 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  def email_list
+    list = self.readers.map {|reader| reader.email}
+    list.join(";")
+  end 
+
+  def self.most_popular
+    self.all.max_by {|magazine| magazine.subscriptions.count}
+  end 
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,16 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        Subscription.create(magazine_id: magazine.id, price: price, reader_id: self.id)
+    end 
+
+    def total_subscription_price
+        self.subscriptions.sum(:price)
+    end 
+
+    def cancel_subscription(magazine)
+        self.subscriptions.find_by(magazine_id: magazine.id).destroy 
+    end 
 end
